30586fcf9b23ab8517db0ae1f1517517d0cd6f1c
Payton Fizer <>
2021-05-13 15:38:38 +0000

    Made Pyramid for proj 1 but backwards and added the HTML for proj 2

--- mario.js
+++ mario.js
@@ -7,22 +7,33 @@ let rows = 5
 let str = '';
 
 
-for ( let i = 2; i <= rows+1; i++){  
-  str = " ";
-  for(let j=1; j<rows-1; j++)
+for ( let i = 1; i <= rows; i++){  
+  str = "";
+  for( let j = 4; j < rows-1; j++)
   {
-    document.write( "&nbsp&nbsp" );
-  for ( j = 6-i; j <= 5; j++ )
+    str += " ";
+  }
+  for ( j = rows-i; j <= 5; j++ )
   {
     str += "#"; 
   }
-  }
+  
 
   console.log(str);
 }
+
+// i = i +1;
+// i++;
 
 function printPyramid(height) {
     console.log("Uh oh... the pyramid is under construction.");
     console.log("Check back soon, our developers are hard at work as we speak!");
 
 }
+
+
+//just an example
+
+// console.log("  ##")
+// console.log(" ###")
+// console.log("####")
