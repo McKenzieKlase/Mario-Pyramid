335bca38785eb0b63488b8b842bfd11eb1437c0e
Payton Fizer <>
2021-05-15 05:14:52 +0000

    Make Pyramid different designs and add a slider, limit to 20 characters high

--- mario.js
+++ mario.js
@@ -1,10 +1,11 @@
 document.getElementById("pyrheight").addEventListener("click", printPyramid);
 
+document.getElementById("brickchoice").addEventListener("click", printPyramid);
 //printPyramid(5);
 
 function printPyramid(height) {
 
-let rows = document.getElementById("height").value;
+let rows = document.getElementById("pyrheight").value;
 
 let str = '';
 
@@ -13,11 +14,11 @@ for ( let i = 1; i <= rows; i++){
   str = "";
   for( let j = 0; j < rows-i; j++)
   {
-    str += ".";
+    str += " ";
   }
   for ( j = rows-i; j <= rows; j++ )
   {
-    str += "#"; 
+    str += document.getElementById("brickchoice").value; 
   }
   
   console.log(str)
